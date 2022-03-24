fea1e04264ab55a5c9d13edd2b342e6b7b4f6c92
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-03-23 22:49:14 -0700

    add about content and another contact button

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,22 +9,28 @@ import {
   Collapse,
   LightMode,
   Tag,
+  VStack,
 } from "@chakra-ui/react";
 import Paragraph from "../components/Paragraph";
 import { useState } from "react";
+import Contact from "../components/Home/Contact";
 
 const About = () => {
   const [show, setShow] = useState(false);
 
   const coolStuff = [
     "React",
-    "Javascript",
-    "AI",
-    "Space",
+    "JavasSript",
+    "Next.js",
+    "Machine Learning",
+    "Cybersecurity",
+    "Physics",
     "Android",
-    "Movies",
     "Figma",
+    "Video Games",
+    "Movies",
     "Coffee",
+    "Lots of coffee",
   ];
 
   const handleToggle = () => setShow(!show);
@@ -46,9 +52,38 @@ const About = () => {
                 About Me
               </Heading>
               <Collapse in={show} startingHeight={100}>
-                <Paragraph fontSize="xl" lineHeight={1.6}>
-                  TODO: write this
-                </Paragraph>
+                <VStack>
+                  <Paragraph fontSize="xl" lineHeight={1.6}>
+                    I'm in the final year of my Bachelor of Software Engineering
+                    degree at the University of Victoria. My professional
+                    interests include cross-platform software development,
+                    cybersecurity, and artificial intelligence. I love to
+                    explore and innovate with new technologies. In my free time
+                    I enjoy working on new software projects (mostly ML),
+                    playing guitar, and going on hikes.
+                  </Paragraph>
+                  <Paragraph fontSize="xl" lineHeight={1.6}>
+                    I'm currently involved in a couple of student clubs:
+                    VikeLabs and Vikaxe. These are clubs for software
+                    development and cybersecurity, respectively. For VikeLabs,
+                    I've had the pleasure of hosting a react testing workshop
+                    and am working on a degree planner web application for UVic
+                    students. With Vikaxe, I've participated in a few CTFs and
+                    have learned a lot.
+                  </Paragraph>
+                  <Paragraph fontSize="xl" lineHeight={1.6}>
+                    My experience in industry has been mostly based on co-ops
+                    (paid internships). It includes multiple roles working as a
+                    full-stack software developer, a software developer in test,
+                    and a front-end software developer. These roles have
+                    provided invaluable experience working in a team environment
+                    and have helped me develop my skills in a variety of areas.
+                  </Paragraph>
+                  <Paragraph fontSize="xl" lineHeight={1.6}>
+                    If you've read this far, feel free to reach out!
+                  </Paragraph>
+                  <Contact />
+                </VStack>
               </Collapse>
               <LightMode>
                 <Button
