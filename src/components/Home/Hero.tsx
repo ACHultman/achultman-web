7332b9ab7d991cf5db9bf10174826c7388ac636e
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-04-01 20:28:22 -0700

    change hero content

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -25,26 +25,10 @@ const Hero = () => {
           Hey, I’m Adam
         </Heading>
         <Paragraph fontSize="2xl" lineHeight={1.6}>
-          Full-stack Developer, Designer, ML Engineer, Cybersecurity Enthusiast.
-          I love using{" "}
-          <Link
-            color="green.500"
-            href="https://www.nodejs.org"
-            isExternal
-            fontWeight="500"
-          >
-            Node.JS
-          </Link>{" "}
-          and{" "}
-          <Link
-            color="green.500"
-            href="https://www.reactjs.org"
-            fontWeight="500"
-            isExternal
-          >
-            React
-          </Link>
-          .
+          Full-stack Developer, Designer, Cybersecurity + ML Enthusiast
+        </Paragraph>
+        <Paragraph fontSize="2xl" lineHeight={1.6}>
+          Learning every day.
         </Paragraph>
         <Contact />
       </Box>
