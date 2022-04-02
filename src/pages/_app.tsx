71ea4275a2f62190b164bc1e412ce6a32f135650
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-04-01 20:35:00 -0700

    add accessibility for reduced motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { DefaultSeo } from "next-seo";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
+import { MotionConfig } from "framer-motion";
+
 import Layout from "../components/Layout";
 import theme from "../theme";
 import SEO from "../next-seo.config";
@@ -12,11 +14,13 @@ import SEO from "../next-seo.config";
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
     <DefaultSeo {...SEO} />
-    <ChakraProvider resetCSS theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <MotionConfig reducedMotion="user">
+      <ChakraProvider resetCSS theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </MotionConfig>
   </>
 );
 
