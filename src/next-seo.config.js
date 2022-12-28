66068e78d3c57825e9593eecd049b2e205111bf4
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-12-27 19:29:10 -0800

    seo edit

--- src/next-seo.config.js
+++ src/next-seo.config.js
@@ -1,18 +1,18 @@
 export default {
   title: "Adam Hultman",
   description:
-    "Adam Hultman | Full-stack developer, UI/UX Designer, security enthusiast.",
+    "Adam Hultman | Full-stack developer, UI/UX Designer, cybersecurity enthusiast.",
   canonical: "https://www.hultman.dev/",
   image: "https://www.hultman.dev/images/hultman-og.png",
   openGraph: {
     url: "https://www.hultman.dev/",
     title: "Adam Hultman",
     description:
-      "Adam Hultman | Full-stack developer, UI/UX Designer, security enthusiast.",
+      "Adam Hultman | Full-stack developer, UI/UX Designer, cybersecurity enthusiast.",
     site_name: "Adam Hultman",
   },
   twitter: {
     handle: "@HultmanAdam",
     cardType: "summary_large_image",
   },
-};
+}
