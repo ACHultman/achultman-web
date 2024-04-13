006549eb56f01004aa8458121909903dceea9c76
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2024-04-13 12:47:32 -0700

    bump packages

--- src/pages/api/v2/chat.ts
+++ src/pages/api/v2/chat.ts
@@ -1,11 +1,10 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { NextRequest } from 'next/server'
+import OpenAI from 'openai'
 
-// Create an OpenAI API client (that's edge friendly!)
-const config = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })
-const openai = new OpenAIApi(config)
 
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge'
@@ -16,17 +15,16 @@ const CURR_DATE = new Date().toLocaleDateString('en-US', {
     month: 'long',
     day: 'numeric',
 })
-const systemInitMessage = process.env.OPENAI_SYSTEM_INIT_MSG.replace(
-    '{CURR_DATE}',
-    CURR_DATE
-)
+const systemInitMessage = (
+    process.env.OPENAI_SYSTEM_INIT_MSG || 'Today is {CURR_DATE}.'
+).replace('{CURR_DATE}', CURR_DATE)
 
-export default async function handler(req: Request) {
+export default async function handler(req: NextRequest) {
     // Extract the `messages` from the body of the request
-    const { messages } = await req.json()
+    const { messages } = await req.json() // SyntaxError: Unexpected end of JSON input
 
     // Ask OpenAI for a streaming chat completion given the prompt
-    const response = await openai.createChatCompletion({
+    const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         stream: true,
         // add the system init message to the beginning of the conversation
