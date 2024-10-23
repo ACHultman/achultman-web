3eb997a0e263501824ac0fe864d04ced9feab12d
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2024-10-23 13:17:27 -0700

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { MdHome } from 'react-icons/md';
+
+export default function Custom404() {
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            height="64vh"
+            textAlign="center"
+            p={4}
+        >
+            <Heading as="h1" size="2xl" mb={4}>
+                404 - Page Not Found
+            </Heading>
+            <Text fontSize="lg" mb={6}>
+                Oops! The page you're looking for doesn't exist.
+            </Text>
+            <Link href="/">
+                <Button colorScheme="green" leftIcon={<MdHome />}>
+                    Go back home
+                </Button>
+            </Link>
+        </Box>
+    );
+}
