868a923be1f5a8770cdaa8afe036e5553942bd68
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2025-05-13 01:20:31 -0700

    rm extra comment

--- src/services/notion.ts
+++ src/services/notion.ts
@@ -163,29 +163,6 @@ export async function fetchNotion<T extends DatabaseName>(db: T, id: string) {
         const blocks = await notion.blocks.children.list({ block_id: id });
         const page = MAP_DATABASE_CONFIG[db].formatter(pageResponse);
 
-        // const blocks = await getBlocks(id);
-        // Retrieve block children for nested blocks (one level deep), for example toggle blocks
-        // https://developers.notion.com/docs/working-with-page-content#reading-nested-blocks
-        //   const childBlocks = await Promise.all(
-        //     blocks
-        //       .filter((block) => block.has_children)
-        //       .map(async (block) => {
-        //         return {
-        //           id: block.id,
-        //           children: await getBlocks(block.id),
-        //         };
-        //       })
-        //   );
-        //   const blocksWithChildren = blocks.map((block) => {
-        //     // Add child blocks if the block should contain children but none exists
-        //     if (block.has_children && !block[block.type].children) {
-        //       block[block.type]["children"] = childBlocks.find(
-        //         (x) => x.id === block.id
-        //       )?.children;
-        //     }
-        //     return block;
-        //   });
-
         if (!page) {
             return null;
         }
