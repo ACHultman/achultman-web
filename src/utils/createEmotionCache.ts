cdbdf11fa6d8441baa693b6c4f4c5afa68bec297
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2025-05-18 16:34:56 -0700

    fix: adjust emotion cache insertion point type

--- src/utils/createEmotionCache.ts
+++ src/utils/createEmotionCache.ts
@@ -1,7 +1,7 @@
 import createCache from '@emotion/cache';
 
 export default function createEmotionCache() {
-    let insertionPoint: Element | undefined;
+    let insertionPoint: HTMLElement | undefined;
     if (typeof document !== 'undefined') {
         const emotionInsertionPoint = document.querySelector<HTMLMetaElement>(
             'meta[name="emotion-insertion-point"]'
