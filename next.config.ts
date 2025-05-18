9bf81306adf5ac3296060adabc9903dbb5b3d4ae
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2025-05-18 16:39:10 -0700

    chore: add Webpack Bundle Analyzer support & analyze script

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
     serverExternalPackages: ['next-seo'],
     images: {
         remotePatterns: [
