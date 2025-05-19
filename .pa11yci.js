80958c479cd592ab4e36eb0853cb2d57f205d70f
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2025-05-18 19:34:39 -0700

    ci: fix pa11y ci config

--- .pa11yci.js
+++ .pa11yci.js
@@ -1,13 +1,23 @@
+const isCI = process.env.CI === 'true';
+
 module.exports = {
     defaults: {
         standard: 'WCAG2AA',
-        runners: ['axe'],
         chromeLaunchConfig: {
-            executablePath: '/usr/bin/chromium-browser',
+            executablePath: isCI ? undefined : '/usr/bin/chromium-browser',
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--headless',
+            ],
         },
         headers: {
             'x-vercel-protection-bypass':
                 process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
         },
+        timeout: 30000,
     },
+    urls: [isCI ? process.env.VERCEL_PREVIEW_URL : 'http://localhost:3000'],
 };
