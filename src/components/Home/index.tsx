e01439603aa26a3d1e2c5fa336338c901a9739fd
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2025-05-19 02:12:42 -0700

    fix: optimize hiding Homepage dividers while components load

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,53 +1,46 @@
 import { Divider, Flex } from '@chakra-ui/react';
-import { useState } from 'react';
-
 import dynamic from 'next/dynamic';
 
-const Hero = dynamic(() => import('./Hero'));
-const Timeline = dynamic(() => import('./Timeline'));
-const GitTimeline = dynamic(() => import('./GitTimeline'));
-const Chat = dynamic(() => import('@components/Chat'), { ssr: false });
-const Skills = dynamic(() => import('./Skills'));
-const Contact = dynamic(() => import('@components/Contact'));
-import LazySection from './LazySection';
+const loading = () => (
+    <div className="section-loading" style={{ minHeight: 120 }} />
+);
+
+const Hero = dynamic(() => import('./Hero'), {
+    loading,
+});
+const Timeline = dynamic(() => import('./Timeline'), {
+    loading,
+});
+const GitTimeline = dynamic(() => import('./GitTimeline'), {
+    loading,
+});
+const Chat = dynamic(() => import('@components/Chat'), {
+    ssr: false,
+    loading,
+});
+const Skills = dynamic(() => import('./Skills'), {
+    loading,
+});
+const Contact = dynamic(() => import('@components/Contact'), {
+    loading,
+});
 
 function Home() {
-    const [heroLoaded, setHeroLoaded] = useState(false);
-    const [chatLoaded, setChatLoaded] = useState(false);
-    const [skillsLoaded, setSkillsLoaded] = useState(false);
-    const [gitTimelineLoaded, setGitTimelineLoaded] = useState(false);
-    const [timelineLoaded, setTimelineLoaded] = useState(false);
-    const [contactLoaded, setContactLoaded] = useState(false);
-
     return (
         <>
-            <LazySection onLoaded={() => setHeroLoaded(true)}>
-                <Hero />
-            </LazySection>
-            {heroLoaded && chatLoaded && <Divider my={10} />}
-            <LazySection onLoaded={() => setChatLoaded(true)}>
-                <Chat />
-            </LazySection>
-            {chatLoaded && skillsLoaded && <Divider my={10} />}
-            <LazySection onLoaded={() => setSkillsLoaded(true)}>
-                <Skills />
-            </LazySection>
-            {skillsLoaded && gitTimelineLoaded && timelineLoaded && (
-                <Divider my={10} />
-            )}
+            <Hero />
+            <Divider className="divider" my={10} />
+            <Chat />
+            <Divider className="divider" my={10} />
+            <Skills />
+            <Divider className="divider" my={10} />
             <Flex justifyContent="center">
-                <LazySection onLoaded={() => setGitTimelineLoaded(true)}>
-                    <GitTimeline />
-                </LazySection>
-                <LazySection onLoaded={() => setTimelineLoaded(true)}>
-                    <Timeline />
-                </LazySection>
+                <GitTimeline />
+                <Timeline />
             </Flex>
-            {timelineLoaded && contactLoaded && <Divider my={10} />}
+            <Divider className="divider" my={10} />
             <section id="contact">
-                <LazySection onLoaded={() => setContactLoaded(true)}>
-                    <Contact />
-                </LazySection>
+                <Contact />
             </section>
         </>
     );
